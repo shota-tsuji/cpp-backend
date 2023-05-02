f36ffd1b526a94fa2a26779184ec137de4b6c01f
shota-tsuji <shotatuzi@gmail.com>
2023-05-02 10:34:47 +0900

    feat: set http server and get endpoint

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,41 @@ use axum::{
 };
 use sqlx::mysql::MySqlPool;
 use std::env;
+use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let pool = MySqlPool::connect(&env::var("DATABASE_URL")?).await?;
-    let recs = sqlx::query!(r#"SELECT id, title, description FROM recipes"#)
-        .fetch_all(&pool)
-        .await?;
+async fn main() {
+    let pool = MySqlPool::connect(&env::var("DATABASE_URL").unwrap())
+        .await
+        .unwrap();
 
-    for rec in recs {
-        println!("{}, {}, {}", rec.id, rec.title, rec.description.unwrap(),);
-    }
+    let app = Router::new()
+        .route("/", get(get_recipe_list))
+        .with_state(pool);
 
-    Ok(())
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn get_recipe_list(State(pool): State<MySqlPool>) -> Result<String, (StatusCode, String)> {
+    let rec = sqlx::query!(r#"SELECT id, title, description FROM recipes"#)
+        .fetch_one(&pool)
+        .await
+        .map_err(internal_error);
+
+    let title = rec.unwrap().title;
+    println!("{}", title);
+
+    Ok(title)
+}
+
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
