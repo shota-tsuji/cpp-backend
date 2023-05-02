a2e577b5d5d77e8c611c069f9a6dc6d7b23be258
shota-tsuji <shotatuzi@gmail.com>
2023-05-03 08:02:04 +0900

    feat: print recipe row

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use async_graphql::{Context, Object};
 use axum::{
     async_trait,
     extract::{FromRef, FromRequestParts, State},
@@ -27,16 +28,26 @@ async fn main() {
         .unwrap();
 }
 
-async fn get_recipe_list(State(pool): State<MySqlPool>) -> Result<String, (StatusCode, String)> {
-    let rec = sqlx::query!(r#"SELECT id, title, description FROM recipes"#)
-        .fetch_one(&pool)
-        .await
-        .map_err(internal_error);
+async fn get_recipe_list(State(pool): State<MySqlPool>) -> Result<(), (StatusCode, String)> {
+    let recipe_row: Option<RecipeRow> =
+        sqlx::query_as("SELECT id, title, description FROM recipes WHERE id = ?")
+            .bind("0")
+            .fetch_optional(&pool)
+            .await
+            .unwrap();
 
-    let title = rec.unwrap().title;
-    println!("{}", title);
+    let recipe = recipe_row
+        .map(|row| Recipe {
+            id: row.id,
+            title: row.title,
+            description: row.description,
+        })
+        .unwrap();
 
-    Ok(title)
+    //let title = rec.unwrap().title;
+    println!("{:?}", &recipe);
+
+    Ok(())
 }
 
 fn internal_error<E>(err: E) -> (StatusCode, String)
@@ -44,4 +55,51 @@ where
     E: std::error::Error,
 {
     (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
+
+#[derive(Debug, async_graphql::SimpleObject)]
+pub struct Recipe {
+    pub id: i32,
+    pub title: String,
+    pub description: String,
+}
+
+pub struct QueryRoot {
+    pool: MySqlPool,
+}
+
+#[derive(sqlx::FromRow)]
+struct RecipeRow {
+    id: i32,
+    title: String,
+    description: String,
+}
+
+#[Object]
+impl QueryRoot {
+    async fn recipe(&self, ctx: &Context<'_>) -> Result<Recipe, String> {
+        //let rec = sqlx::query!(r#"SELECT id, title, description FROM recipes"#)
+        //    .fetch_one(&self.pool)
+        //    .await
+        //    .map_err(internal_error);
+        let recipe_row: Option<RecipeRow> =
+            sqlx::query_as(r#"SELECT id, title, description FROM recipes WHERE id = $1"#)
+                .bind("0")
+                .fetch_optional(&self.pool)
+                .await
+                .unwrap();
+
+        let recipe = recipe_row
+            .map(|row| Recipe {
+                id: row.id,
+                title: row.title,
+                description: row.description,
+            })
+            .unwrap();
+
+        //let title = rec.unwrap().title;
+        println!("{:?}", &recipe);
+
+        Ok(recipe)
+    }
 }
